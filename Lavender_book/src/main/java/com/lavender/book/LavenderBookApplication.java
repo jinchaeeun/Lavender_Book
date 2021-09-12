149a20c862724885f3a4af7d107d284c46a6f5ed
jinchaeeun <kbg05032@naver.com>
2021-09-12 23:03:33 +0900

    init

---
+++ Lavender_book/src/main/java/com/lavender/book/LavenderBookApplication.java
@@ -0,0 +1,13 @@
+package com.lavender.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavenderBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavenderBookApplication.class, args);
+	}
+
+}
---
+++ Lavender_book/src/test/java/com/lavender/book/LavenderBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavender.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavenderBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
